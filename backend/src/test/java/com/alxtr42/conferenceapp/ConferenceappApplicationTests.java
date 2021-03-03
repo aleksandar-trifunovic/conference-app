f1c5d68455efaf9f12f1217516bf7c914c2de3d6
Aleksandar Trifunovic <aleksandar.trifunovic96@outlook.com>
2021-03-03 01:05:11 +0100

    Spring init

---
+++ backend/src/test/java/com/alxtr42/conferenceapp/ConferenceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alxtr42.conferenceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
